58040b54fffd711145fd52935ad49783df47aed8
Oleg <olegigar@mail.ru>
2019-11-07 22:09:33 +0700

    Добавил MuseumObserverLogic: Transaction и Connection

---
+++ MuseumObserverLogic/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuseumObserverLogic
+{
+    class Connection
+    {
+        private System.Data.SqlClient.SqlConnection Conn;
+
+        public Connection()
+        {
+            var connectionString = @"Data Source = (local)\SQLEXPRESS; Initial Catalog = MuseumDB; Integrated Security = True";
+            Conn = new SqlConnection(connectionString);
+        }
+        public void Open() { Conn.Open(); }
+        public void Close() { Conn.Close(); }
+        public Transaction BeginTransaction()
+        {
+            System.Data.SqlClient.SqlTransaction tr = Conn.BeginTransaction();
+            Transaction result = new Transaction(tr);
+            return result;
+        }
+        public System.Data.SqlClient.SqlConnection getConnection() { return Conn; }
+    }
+}
---
+++ MuseumObserverLogic/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuseumObserverLogic
+{
+    class Transaction
+    {
+        private System.Data.SqlClient.SqlTransaction Tr;
+        public Transaction(SqlTransaction t) { Tr = t; }
+        public void Commit() { Tr.Commit(); }
+        public void Rollback() { Tr.Rollback(); }
+        public System.Data.SqlClient.SqlTransaction getTransaction() { return Tr; }
+    }
+}
